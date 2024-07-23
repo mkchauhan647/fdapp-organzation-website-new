e9b510663c4e30a3a346ba0b8feb7496d45d61e6
saniz7 <sanizsanij@gmail.com>
2024-07-23 10:21:09 +0545

    fix:coupon design change

--- src/helpers/ui/Coupan.tsx
+++ src/helpers/ui/Coupan.tsx
@@ -1,36 +1,53 @@
-import Image from 'next/image';
-import React from 'react';
-import { PayModal } from './PayModal';
-import { Coupon } from '@/utils/schema/ApiInterface';
+import Image from "next/image";
+import React, { useState } from "react";
+import { PayModal } from "./PayModal";
+import { Coupon } from "@/utils/schema/ApiInterface";
 
 const Coupan: React.FC<Coupon> = ({ ...coupon }) => {
-    return (
-        <>
-            <div className='coupon-box flex flex-col gap-2 py-2 px-3 bg-white rounded-lg'>
-                <div className='flex justify-between items-center'>
-                    <Image src='/image/vote/cupon.png' height={500} width={900} alt='cuopn' className='h-[3rem] w-[3rem] object-contain' />
-                    <div>
-                        <h3 className='font-secular font-[500] text-lg text-[var(--black)] text-right'>{coupon.name}</h3>
-                        <p className='font-secular font-[500] text-lg text-[var(--blue)] text-right'>${coupon.pricing}</p>
-                    </div>
-                </div>
+  const [showModal, setShowModal] = useState(false);
 
-                <div className='text-xs font-[500] text-[var(--c-secondary)] flex justify-between items-center gap-[10px]'>
-                    <div className='bg-[var(--c-rose-pink)] px-2 py-1 rounded-lg'>
-                        <p>{coupon.votes} votes</p>
-                    </div>
-                    <div className='bg-[var(--c-rose-pink)] text-[var(--c-secondary)] px-2 py-1 rounded-lg'>
-                        <p>{coupon.eligibleCandidateCounts} Candidates</p>
-                    </div>
-                </div>
+  const handleNameClick = () => {
+    setShowModal(true);
+  };
 
-                <div>
-                    <PayModal {...coupon} />
-                </div>
+  return (
+    <>
+      <div
+        className="coupon-box flex items-center justify-center flex-col py-2 px-3 bg-white rounded-lg h-20"
+        onClick={handleNameClick}
+      >
+        <div className="flex justify-between items-center gap-4">
+          {/* <Image
+            src="/image/vote/cupon.png"
+            height={500}
+            width={900}
+            alt="cuopn"
+            className="h-[3rem] w-[3rem] object-contain"
+          /> */}
+          <h3 className="font-secular font-[500] text-lg text-[var(--black)] text-right cursor-pointer">
+            {coupon.name}
+          </h3>
+          {showModal && (
+            <p className="font-secular font-[500] text-lg bg-[var(--btncolor)] text-white p-1 px-1 rounded-lg text-right">
+              NPR.{coupon.pricing}
+            </p>
+          )}
+        </div>
 
-            </div>
-        </>
-    );
-}
+        {/* <div className="text-xs font-[500] text-[var(--c-secondary)] flex justify-between items-center gap-[10px]">
+          <div className="bg-[var(--c-rose-pink)] px-2 py-1 rounded-lg">
+            <p>{coupon.votes} votes</p>
+          </div>
+          <div className="bg-[var(--c-rose-pink)] text-[var(--c-secondary)] px-2 py-1 rounded-lg">
+            <p>{coupon.eligibleCandidateCounts} Candidates</p>
+          </div>
+        </div> */}
+
+        <div></div>
+        {showModal && <PayModal {...coupon} />}
+      </div>
+    </>
+  );
+};
 
 export default Coupan;
